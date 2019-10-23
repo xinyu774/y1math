6b44930ee02f643b14f123c2fc32838939bbd00d
xinyu774 <52947158+xinyu774@users.noreply.github.com>
2019-10-23 13:55:04 +0800

    Update t01primeshcflcm_evennumbers.py

--- t01primeshcflcm_evennumbers.py
+++ t01primeshcflcm_evennumbers.py
@@ -4,3 +4,18 @@ num = 123
 for i in range(1, num+1):
   if i % 2 == 0:
     print(i, end=' ')
+# Python program to print Even Numbers in a List 
+  
+# list of numbers 
+list1 = [10, 21, 4, 45, 66, 93] 
+num = 0
+  
+# using while loop         
+while(num < len(list1)): 
+      
+    # checking condition 
+    if num % 2 == 0: 
+       print(list1[num], end = " ") 
+      
+    # increment num   
+    num += 1
